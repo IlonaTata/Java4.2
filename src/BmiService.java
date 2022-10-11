3c3a9397c14b02fb658d5da1d2b90bbeb3547759
Ilona Tatarenko <ilona-tatarenko@mail.ru>
2022-10-11 15:16:40 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,14 @@
+public class BmiService {
+    public double calculate(int m, double h) {
+
+
+        double indeks;
+        double hh;
+        hh = h*h;
+
+        indeks = m/hh;
+    return indeks;}
+
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        BmiService service = new BmiService();
+        double indeks = service.calculate (70, 1.7);
+
+        System.out.println( indeks);
+    }
+
+
+}
